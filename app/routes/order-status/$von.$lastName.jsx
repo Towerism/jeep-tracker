b576d72fafcdb0705900b430ea757ab078ea9e6e
Martin Fracker, Jr <martinfrackerjr@gmail.com>
2022-08-25 23:23:25 -0500

    order status: add von and vin

--- app/routes/order-status/$von.$lastName.jsx
+++ app/routes/order-status/$von.$lastName.jsx
@@ -1,6 +1,5 @@
-import { Box } from "@mui/material";
+import { Box, Card, CardContent, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
-import { Container } from "@mui/system";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
@@ -12,17 +11,44 @@ export const loader = async ({ params }) => {
   return json(result);
 };
 
+function BasicStatusCard({ title, children }) {
+  return (
+    <Card sx={{ minWidth: 150 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="h5" component="div">
+          {children}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
 export default function OrderStatus() {
-  const { image, statusCode, statusDesc } = useLoaderData();
+  const { image, statusCode, statusDesc, vehicle, von, vin } = useLoaderData();
   return (
     <main>
       <Box>
         <Grid container>
-          <Grid sx={{textAlign: "center"}}>
-            <img src={image} style={{width: "90%"}} />
+          <Grid sx={{ textAlign: "center" }}>
+            <img src={image} alt={vehicle} style={{ width: "90%" }} />
           </Grid>
           <Grid>
-            {statusCode}: {statusDesc}
+            <Grid container>
+              <Grid>
+                <BasicStatusCard title="Order number">{von}</BasicStatusCard>
+              </Grid>
+              <Grid>
+                <BasicStatusCard title="Order status">
+                  {statusCode} - {statusDesc}
+                </BasicStatusCard>
+              </Grid>
+              <Grid>
+                <BasicStatusCard title="VIN">{vin}</BasicStatusCard>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Box>
