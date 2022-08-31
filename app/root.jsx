83d755f06746cd51c85126fc93c67e3ab2c0e6a4
Martin Fracker, Jr <martinfrackerjr@gmail.com>
2022-08-31 17:05:17 -0500

    Better 404 handling

--- app/root.jsx
+++ app/root.jsx
@@ -12,7 +12,12 @@ import {
   useLocation,
 } from "@remix-run/react";
 import { withEmotionCache } from "@emotion/react";
-import { unstable_useEnhancedEffect } from "@mui/material";
+import {
+  Box,
+  Link,
+  Typography,
+  unstable_useEnhancedEffect,
+} from "@mui/material";
 import theme from "./src/theme";
 import ClientStyleContext from "./src/ClientStyleContext";
 import Layout from "./src/Layout";
@@ -25,62 +30,67 @@ export const loader = async () => {
 const useEnhancedEffect =
   typeof window !== "undefined" ? unstable_useEnhancedEffect : React.useEffect;
 
-const Document = withEmotionCache(({ children, title }, emotionCache) => {
-  const clientStyleData = React.useContext(ClientStyleContext);
-  const location = useLocation();
-  const { gaTrackingId } = useLoaderData();
-
-  // Only executed on client
-  useEnhancedEffect(() => {
-    // re-link sheet container
-    emotionCache.sheet.container = document.head;
-    // re-inject tags
-    const tags = emotionCache.sheet.tags;
-    emotionCache.sheet.flush();
-    tags.forEach((tag) => {
-      // eslint-disable-next-line no-underscore-dangle
-      emotionCache.sheet._insertTag(tag);
-    });
-    // reset cache to reapply global styles
-    clientStyleData.reset();
-  }, []);
-
-  React.useEffect(() => {
-    if (gaTrackingId?.length) {
-      gtag.pageview(location.pathname, gaTrackingId);
+const Document = withEmotionCache(
+  ({ children, title, isError }, emotionCache) => {
+    const clientStyleData = React.useContext(ClientStyleContext);
+    const location = useLocation();
+    let gaTrackingId = "";
+    if (!isError) {
+      const data = useLoaderData();
+      gaTrackingId = data?.gaTrackingId ?? "";
     }
-  }, [location, gaTrackingId]);
 
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="theme-color" content={theme.palette.primary.main} />
-        {title ? <title>{title}</title> : null}
-        <Meta />
-        <Links />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-        <meta
-          name="emotion-insertion-point"
-          content="emotion-insertion-point"
-        />
-      </head>
-      <body>
-        {process.env.NODE_ENV === "development" || !gaTrackingId ? null : (
-          <>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
-            />
-            <script
-              async
-              id="gtag-init"
-              dangerouslySetInnerHTML={{
-                __html: `
+    // Only executed on client
+    useEnhancedEffect(() => {
+      // re-link sheet container
+      emotionCache.sheet.container = document.head;
+      // re-inject tags
+      const tags = emotionCache.sheet.tags;
+      emotionCache.sheet.flush();
+      tags.forEach((tag) => {
+        // eslint-disable-next-line no-underscore-dangle
+        emotionCache.sheet._insertTag(tag);
+      });
+      // reset cache to reapply global styles
+      clientStyleData.reset();
+    }, []);
+
+    React.useEffect(() => {
+      if (gaTrackingId?.length) {
+        gtag.pageview(location.pathname, gaTrackingId);
+      }
+    }, [location, gaTrackingId]);
+
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          {title ? <title>{title}</title> : null}
+          <Meta />
+          <Links />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <meta
+            name="emotion-insertion-point"
+            content="emotion-insertion-point"
+          />
+        </head>
+        <body>
+          {process.env.NODE_ENV === "development" || !gaTrackingId ? null : (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+              />
+              <script
+                async
+                id="gtag-init"
+                dangerouslySetInnerHTML={{
+                  __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
@@ -88,18 +98,19 @@ const Document = withEmotionCache(({ children, title }, emotionCache) => {
                   page_path: window.location.pathname,
                 });
               `,
-              }}
-            />
-          </>
-        )}
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
-});
+                }}
+              />
+            </>
+          )}
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    );
+  }
+);
 
 // https://remix.run/api/conventions#default-export
 // https://remix.run/api/conventions#route-filenames
@@ -115,8 +126,9 @@ export default function App() {
 
 // https://remix.run/docs/en/v1/api/conventions#errorboundary
 export function ErrorBoundary({ error }) {
+  console.log(error);
   return (
-    <Document title="Error!">
+    <Document title="Error!" isError={true}>
       <Layout>
         <div>
           <h1>There was an error.</h1>
@@ -156,10 +168,13 @@ export function CatchBoundary() {
   return (
     <Document title={`${caught.status} ${caught.statusText}`}>
       <Layout>
-        <h1>
-          {caught.status}: {caught.statusText}
-        </h1>
-        {message}
+        <Box sx={{ textAlign: "center" }}>
+          <h1>
+            {caught.status}: {caught.statusText}
+          </h1>
+          <Typography variant="body1">{message}</Typography>
+          <Link href="/">Take me home!</Link>
+        </Box>
       </Layout>
     </Document>
   );
