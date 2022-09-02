9b7fe9034ba041f0ba3921f7aecd39c4f98de87b
Martin Fracker, Jr <martinfrackerjr@gmail.com>
2022-09-01 21:13:13 -0500

    Maybe improve screenshot load time

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  ...(process.env === "production" && { server: "./server.js" }),
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
