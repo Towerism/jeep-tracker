913a945a8220a4493ec494e7b4f61b56568885f1
Martin Fracker, Jr <martinfrackerjr@gmail.com>
2022-09-01 22:58:32 -0500

    Add button to download screenshot

--- app/routes/screenshot/$von.$lastName.jsx
+++ app/routes/screenshot/$von.$lastName.jsx
@@ -1,7 +1,8 @@
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useParams } from "@remix-run/react";
 import { json } from "@remix-run/node";
 import { getScreenshot } from "~/models/screenshot.server";
 import { Box } from "@mui/system";
+import { Button, Card, CardContent, Typography } from "@mui/material";
 
 export const loader = async ({ params }) => {
   const { von, lastName } = params;
@@ -11,9 +12,35 @@ export const loader = async ({ params }) => {
 
 export default function Screenshot() {
   const { dataUrl } = useLoaderData();
+  const { von, lastName } = useParams();
+
+  const downloadScreenshot = () => {
+    var link = document.createElement("a");
+    link.download = `screenshot_${von}_${lastName}}.jpg`;
+    link.href = dataUrl;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <Box sx={{ textAlign: "center" }}>
-      <img src={dataUrl} alt="screenshot" />
+      <Card>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Screenshot
+          </Typography>
+          <Typography variant="body1" sx={{ mb: 2 }}>
+            Below is a screenshot of your order status.
+          </Typography>
+          <Button onClick={downloadScreenshot} sx={{ mb: 2 }}>
+            Download
+          </Button>
+          <Box>
+            <img src={dataUrl} alt="screenshot" />
+          </Box>
+        </CardContent>
+      </Card>
     </Box>
   );
 }
