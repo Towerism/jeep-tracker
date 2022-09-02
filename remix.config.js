db4f60c9fcfe2fa3f6b95e74446643e60db62bb2
Martin Fracker, Jr <martinfrackerjr@gmail.com>
2022-09-01 21:21:57 -0500

    Revert "Maybe improve screenshot load time"
    This reverts commit 9b7fe9034ba041f0ba3921f7aecd39c4f98de87b.

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  ...(process.env === "production" && { server: "./server.js" }),
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
