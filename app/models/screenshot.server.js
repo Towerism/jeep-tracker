fea78952495c04a8caeb285fdf622e633eddb21b
Martin Fracker, Jr <martinfrackerjr@gmail.com>
2022-09-05 00:39:40 -0500

    screenshot: use micro service for screenshots

--- app/models/screenshot.server.js
+++ app/models/screenshot.server.js
@@ -1,61 +1,11 @@
-import puppeteer from "puppeteer";
+import axios from "axios";
 
 export async function getScreenshot(von, lastName) {
   const baseUrl =
     process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : "https://jeeponorder.com";
+      ? "http://localhost:3001"
+      : "https://jeep-tracker-screenshot.fly.dev";
 
-  const browser = await puppeteer.launch({
-    userDataDir: ".puppeteercache",
-    headless: true,
-    args: [
-      "--no-sandbox",
-      "--autoplay-policy=user-gesture-required",
-      "--disable-background-networking",
-      "--disable-background-timer-throttling",
-      "--disable-backgrounding-occluded-windows",
-      "--disable-breakpad",
-      "--disable-client-side-phishing-detection",
-      "--disable-component-update",
-      "--disable-default-apps",
-      "--disable-dev-shm-usage",
-      "--disable-domain-reliability",
-      "--disable-extensions",
-      "--disable-features=AudioServiceOutOfProcess",
-      "--disable-hang-monitor",
-      "--disable-ipc-flooding-protection",
-      "--disable-notifications",
-      "--disable-offer-store-unmasked-wallet-cards",
-      "--disable-popup-blocking",
-      "--disable-print-preview",
-      "--disable-prompt-on-repost",
-      "--disable-renderer-backgrounding",
-      "--disable-setuid-sandbox",
-      "--disable-speech-api",
-      "--disable-sync",
-      "--hide-scrollbars",
-      "--ignore-gpu-blacklist",
-      "--metrics-recording-only",
-      "--mute-audio",
-      "--no-default-browser-check",
-      "--no-first-run",
-      "--no-pings",
-      "--no-sandbox",
-      "--no-zygote",
-      "--password-store=basic",
-      "--use-gl=swiftshader",
-      "--use-mock-keychain",
-    ],
-    timeout: 10000,
-  });
-  const page = await browser.newPage();
-  const dimension = 1300;
-  await page.setViewport({ width: dimension, height: dimension });
-  await page.goto(`${baseUrl}/order-status/${von}/${lastName}?screenshot=true`);
-  await page.waitForSelector("#screenshot-hook");
-  const screenshotHook = await page.$("#screenshot-hook");
-  const base64 = await screenshotHook.screenshot({ encoding: "base64" });
-  await browser.close();
-  return `data:image/jpeg;base64,${base64}`;
+  const { data } = await axios.get(`${baseUrl}/screenshot/${von}/${lastName}`);
+  return data.dataUrl;
 }
