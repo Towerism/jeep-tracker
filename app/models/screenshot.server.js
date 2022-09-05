8e5469e0f02dd5bd44d53fbb7f3bdf98a71c2b66
Martin Fracker, Jr <martinfrackerjr@gmail.com>
2022-09-05 01:21:15 -0500

    screenshot: catch axios error

--- app/models/screenshot.server.js
+++ app/models/screenshot.server.js
@@ -6,6 +6,10 @@ export async function getScreenshot(von, lastName) {
       ? "http://localhost:3001"
       : "https://jeep-tracker-screenshot.fly.dev";
 
-  const { data } = await axios.get(`${baseUrl}/screenshot/${von}/${lastName}`);
-  return data.dataUrl;
+  try {
+    const data = await axios.get(`${baseUrl}/screenshot/${von}/${lastName}`);
+    return data.dataUrl;
+  } catch (_) {
+    throw new Response("There was an error getting the screenshot.", 503);
+  }
 }
