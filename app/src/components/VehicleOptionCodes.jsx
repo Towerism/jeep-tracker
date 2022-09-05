625cb2aec31df39e47a37841f2a5852e8215f12f
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2022-09-04 20:37:49 -0500

    Get basic table working

--- app/src/components/VehicleOptionCodes.jsx
+++ app/src/components/VehicleOptionCodes.jsx
@@ -6,6 +6,7 @@ import {
   ButtonGroup,
   Button,
 } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import Grid from "@mui/material/Unstable_Grid2";
 import { useState } from "react";
 import { useIsScreenshot } from "~/src/hooks/useIsScreenshot";
@@ -31,6 +32,7 @@ function OptionChip({ decoded, showDecoded, code, isSubOption }) {
 
 export function VehicleOptionCodes({ rpoCodes = [] }) {
   const [showDecoded, setShowDecoded] = useState(true);
+  const [showTable, setShowTable] = useState(false);
 
   const isScreenshot = useIsScreenshot();
 
@@ -38,6 +40,20 @@ export function VehicleOptionCodes({ rpoCodes = [] }) {
   if (showDecoded) {
     codes = rpoCodes.filter(({ decoded }) => !!decoded);
   }
+
+  const columns = [
+    {
+      field: "code",
+      headerName: "Option",
+      width: 100,
+    },
+    {
+      field: "decoded",
+      headerName: "Description",
+      width: 400,
+    },
+  ];
+
   return (
     <Grid container sx={isScreenshot ? { mt: -10 } : {}}>
       <Grid sx={{ my: 2 }} xs={12}>
@@ -47,6 +63,10 @@ export function VehicleOptionCodes({ rpoCodes = [] }) {
         {!isScreenshot && (
           <Box align="center">
             <Switch
+              label="Show table view"
+              onChange={(event) => setShowTable(event.target.checked)}
+            />
+            <Switch
               label="Show order guide options only"
               defaultChecked
               onChange={(event) => setShowDecoded(event.target.checked)}
@@ -54,42 +74,58 @@ export function VehicleOptionCodes({ rpoCodes = [] }) {
           </Box>
         )}
       </Grid>
-      <Grid container spacing={1.5} justifyContent="center" xs={12}>
-        {!codes.length &&
-          [...Array(20).keys()].map((i) => (
-            <Skeleton
-              key={i}
-              sx={{ mr: 1.5, mb: 1.5 }}
-              variant="rounded"
-              animation="wave"
-              width={170}
-              height={26}
-            />
-          ))}
-        {codes.map(({ code, decoded, display, isSubOption }) => (
-          <Grid key={code}>
-            {decoded && !showDecoded ? (
-              <Tooltip title={display} placement="top">
-                <Box>
-                  <OptionChip
-                    decoded
-                    code={code}
-                    showDeocded={showDecoded}
-                    isSubOption={isSubOption}
-                  />
-                </Box>
-              </Tooltip>
-            ) : (
-              <OptionChip
-                decoded={decoded}
-                code={code}
-                showDecoded={showDecoded}
-                isSubOption={isSubOption}
+      {!showTable && (
+        <Grid container spacing={1.5} justifyContent="center" xs={12}>
+          {!codes.length &&
+            [...Array(20).keys()].map((i) => (
+              <Skeleton
+                key={i}
+                sx={{ mr: 1.5, mb: 1.5 }}
+                variant="rounded"
+                animation="wave"
+                width={170}
+                height={26}
               />
-            )}
+            ))}
+          {codes.map(({ code, decoded, display, isSubOption }) => (
+            <Grid key={code}>
+              {decoded && !showDecoded ? (
+                <Tooltip title={display} placement="top">
+                  <Box>
+                    <OptionChip
+                      decoded
+                      code={code}
+                      showDeocded={showDecoded}
+                      isSubOption={isSubOption}
+                    />
+                  </Box>
+                </Tooltip>
+              ) : (
+                <OptionChip
+                  decoded={decoded}
+                  code={code}
+                  showDecoded={showDecoded}
+                  isSubOption={isSubOption}
+                />
+              )}
+            </Grid>
+          ))}
+        </Grid>
+      )}
+      {showTable && (
+        <Grid container xs={12}>
+          <Grid xs={12}>
+            <Box sx={{ height: 400, width: "100%", backgroundColor: "#fff" }}>
+              <DataGrid
+                rows={codes}
+                columns={columns}
+                rowsPerPageOptions={[5, 10, 25, 50, 100]}
+                getRowId={({ code }) => code}
+              />
+            </Box>
           </Grid>
-        ))}
-      </Grid>
+        </Grid>
+      )}
     </Grid>
   );
 }
